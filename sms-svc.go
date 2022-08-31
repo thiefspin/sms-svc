54c3915fef0ded86800aada506192db17a2b2424
michael <michael.howard.h@gmail.com>
2022-08-31 22:08:45 +0200

    Move some functions into modules

--- sms-svc.go
+++ sms-svc.go
@@ -2,20 +2,13 @@ package main
 
 import (
 	"bytes"
-	"encoding/json"
-	"fmt"
 	"github.com/gorilla/mux"
-	"io"
 	"log"
 	"math"
 	"net/http"
+	"sms-svc/restutils"
 	"time"
 )
-
-type APIError struct {
-	StatusCode int    `json:"statusCode"`
-	Message    string `json:"message"`
-}
 
 type SMSRequest struct {
 	Sender   string `json:"sender"`
@@ -29,35 +22,9 @@ func (sms *SMSRequest) valid() bool {
 
 var smsQueue = make(chan SMSRequest, math.MaxInt16)
 
-func respond(o any, statusCode int, w http.ResponseWriter) {
-	w.WriteHeader(statusCode)
-	withJsonEncoding(o, func(b *bytes.Buffer) {
-		fmt.Fprintf(w, b.String())
-	})
-}
-
-func withJsonDecoding(body io.ReadCloser, target any, w http.ResponseWriter, f func()) {
-	jsonErr := json.NewDecoder(body).Decode(&target)
-	if jsonErr != nil {
-		res := APIError{StatusCode: 400, Message: "Invalid JSON input"}
-		respond(res, 400, w)
-	} else {
-		f()
-	}
-}
-
-func withJsonEncoding(o any, f func(b *bytes.Buffer)) {
-	jsonObject, err := json.Marshal(o)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	f(bytes.NewBuffer(jsonObject))
-}
-
 func callSMSGateway(sms SMSRequest) bool {
 	success := true
-	withJsonEncoding(sms, func(b *bytes.Buffer) {
+	utils.WithJsonEncoding(sms, func(b *bytes.Buffer) {
 		resp, err := http.Post("http://localhost:4000", "application/json", b)
 		if err != nil || resp.StatusCode == 429 {
 			log.Println("Failed to call SMS API")
@@ -72,14 +39,14 @@ func callSMSGateway(sms SMSRequest) bool {
 
 func create(w http.ResponseWriter, r *http.Request) {
 	var sms SMSRequest
-	withJsonDecoding(r.Body, &sms, w, func() {
+	utils.WithJsonDecoding(r.Body, &sms, w, func() {
 		if !sms.valid() {
-			res := APIError{StatusCode: 400, Message: "Invalid JSON input"}
-			respond(res, 400, w)
+			res := utils.APIError{StatusCode: 400, Message: "Invalid JSON input"}
+			utils.Respond(res, 400, w)
 		} else {
 			log.Println("Server received SMS request: ", sms)
 			smsQueue <- sms
-			respond(sms, 202, w)
+			utils.Respond(sms, 202, w)
 		}
 	})
 }
